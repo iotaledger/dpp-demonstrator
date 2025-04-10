a224fcfa76174060ff44096f44c49ce3054a8d26
Valerio Mellini <valerio@kchain.solutions>
2025-04-10 12:23:36 +0100

    WIP on feature/ui-improvements
    fix: compatibility check with v0.11.6-rc

--- scripts-sh/new_product.sh
+++ scripts-sh/new_product.sh
@@ -1,23 +1,53 @@
-#!/bin/bash
+ #!/bin/bash
 
-AUDIT_TRAIL_PACKAGE_ADDRESS=0xc2b3ad97c8b72c9a76b4963c47f6737b5d1d39aa24474852d909fa895793937f
-FEDERATION_ADDR=0xcd874bde79e99bab7bc736949e65b618715a0fa7a447b8318b44cf3c893fdd98
-SERIAL_NUMBER=abc-12345
-IMAGE_URL=https://assets.xboxservices.com/assets/fa/b8/fab86afd-63d6-45a9-862f-798e5e45cda2.jpg?n=111101_Gallery-0_4_1350x759.jpg
-CLOCK_ADDRESS=0x6
+FEDERATION_OBJ_ID=0xbb42be8c968aa1eabc9d240df04c49e15abc5485e3de9eaf8eb7ee1ceffae46a 
+AUDIT_TRAIL_PACKAGE_ADDRESS=0x4ea3a4ee137ace4c9174d0b354f233540406eb9cca3cbc54d76f213fb83b94ce
+SERIAL_NUMBER=EY74A2-LJ2G-001
+PRODUCT_NAME="Cordless Drill & Driver EY74A2"
+GTIN="5025232813723"
+MANUFACTURER="Panasonic"
+IMAGE_URL="https://www.powertoolsdirect.com/media/catalog/product/cache/926507dc7f93631a094422215b778fe0/p/a/panasonic-ey74a2x-cordless-combi-drill-18v-body-only-ey74a2x57.jpg"
+CLOCK=0x6
 GAS_BUDGET=500000000
 
+COMPONENT_LABELS='[
+  "Product",
+  "Manufacturer",
+  "GTIN",
+  "Motor",
+  "Chuck",
+  "Battery Pack",
+  "Gearbox",
+  "Housing",
+  "LED Light",
+  "Belt Hook",
+  "Carrying Case"
+]'
 
+COMPONENT_DESCRIPTIONS='[
+  "'"$PRODUCT_NAME"'",
+  "'"$MANUFACTURER"'",
+  "'"$GTIN"'",
+  "Brushless DC motor, 32 Nm (soft), 50 Nm (hard), 30–1,580 rpm range",
+  "13 mm keyless chuck, single-sleeve, metal with spindle lock",
+  "18V Li-ion battery pack, 3.0 Ah (EY9L53B) / 5.0 Ah (EY9L54B)",
+  "2-speed gearbox with electronic speed control",
+  "Compact housing (178 mm), ABS/polycarbonate blend, ergonomic grip",
+  "Front-facing LED work light, auto-off after 5 s",
+  "Stainless steel belt hook, ambidextrous, detachable",
+  "PP carrying case with accessory slots and locking latches"
+]'
+
+# Esecuzione della chiamata IOTA
 iota client call \
   --package "$AUDIT_TRAIL_PACKAGE_ADDRESS" \
   --module "app" \
   --function "new_product" \
   --args \
-    $FEDERATION_ADDR \
+    $FEDERATION_OBJ_ID \
     "$SERIAL_NUMBER" \
     "$IMAGE_URL" \
-    '["Product", "Manufacturer", "Battery Pack", "Plastic Shell", "Screws and Fasteners", "Main PCB"]' \
-    '["Xbox Wireless Controller", "Microsoft", "Rechargeable lithium-ion battery", "ABS injection-molded casing (top and bottom)", "Standard Torx screws and internal mounts", "Custom printed circuit board with integrated microcontroller"]' \
-    $CLOCK_ADDRESS \
+    "$COMPONENT_LABELS" \
+    "$COMPONENT_DESCRIPTIONS" \
+    $CLOCK \
   --gas-budget "$GAS_BUDGET"
-

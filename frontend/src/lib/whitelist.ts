b0c27bd925428e0dc5665cc08dc7ef208cf9950e
Valerio Mellini <valerio@kchain.solutions>
2025-04-28 18:36:35 +0200

    fix: lint disabled

--- frontend/src/lib/whitelist.ts
+++ frontend/src/lib/whitelist.ts
@@ -1,3 +1,4 @@
+/* eslint-disable padding-line-between-statements */
 export function isAddressInWhitelist(content: unknown, address: string){
     console.log(JSON.stringify(content))
     return false

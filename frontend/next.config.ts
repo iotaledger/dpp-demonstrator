5ae60a7a4b04649f0ad60d1a832f2dadeb1ca07a
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2025-09-18 21:10:27 +0100

    chore: configure nextjs to copy the required wasm files from @iota/identity-wasm

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,77 @@
 import type { NextConfig } from "next";
+import fs, { access } from "node:fs/promises";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      layers: true,
+    };
+
+    /**
+     * NOTE: Webpack doesn't recognize the necessity to source the WebAssembly asset, even if it recognizes
+     * it is required to the codebase.
+     * - I solved this problem by writing the Webpack plugin CopyFileWebpackPlugin, which copies a file from
+     *   to a target to a destination.
+     * - If the problem turns to happen or it becomes insuficient refer to following issues:
+     *   - https://github.com/vercel/next.js/issues/25852
+     *   - https://github.com/vercel/next.js/discussions/35637
+     */
+    config.plugins.push(
+      new CopyFileWebpackPlugin({
+        from: path.resolve(__dirname, 'node_modules/@iota/identity-wasm/node/identity_wasm_bg.wasm'),
+        to: path.resolve(__dirname, '.next/server/app/api/verify-domain-linkage/identity_wasm_bg.wasm'),
+      }, { isServer }),
+    );
+    return config;
+  },
 };
+
+/**
+ * Copy a file `from ` a target `to` a destination only if the build is targeting
+ * the server, otherwise do nothing.
+ */
+class CopyFileWebpackPlugin {
+  from;
+  to;
+  options;
+
+  static defaultOptions = {
+    isServer: false,
+  };
+
+  constructor(intruction = { from: '', to: '' }, options = {}) {
+    this.from = intruction.from;
+    this.to = intruction.to;
+    this.options = { ...CopyFileWebpackPlugin.defaultOptions, ...options };
+  }
+
+  apply(compiler: any) {
+    compiler.hooks.afterEmit.tapPromise(
+      'CopyFileWebpackPlugin',
+      async (_: unknown) => {
+        if (this.options.isServer) {
+          try {
+            await access(this.to);
+            console.log(`\t[CopyFileWebpackPlugin] ✓ ${this.to} already exists`);
+            return;
+          } catch (error: any) {
+            if (error.code === 'ENOENT') {
+              // No file exists
+            } else {
+              throw error;
+            }
+          }
+
+          await fs.copyFile(this.from, this.to);
+          console.log(`\t[CopyFileWebpackPlugin] ✓ file copied from ${this.from} to ${this.to}`);
+        }
+      },
+    );
+  }
+}
 
 export default nextConfig;

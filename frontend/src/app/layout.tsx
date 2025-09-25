2ef8918c74f47cb7a95789ccf5f48886ccbbc0cf
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2025-09-25 13:42:27 +0100

    chore: setup head metadata, favicon and robot config

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Metadata } from 'next';
 import "./globals.css";
 import { AppLayout } from '@/components/AppLayout';
 
+export const metadata: Metadata = {
+  title: "DPP with IOTA Trust Framework",
+  description: "Explore the Digital Product Passport with IOTA Trust Framework",
+  // See documentation at: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#icons
+  icons: {
+    icon: '/assets/favicon-32x32.webp',
+  },
+  // See documentation at: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#robots
+  robots: {
+    index: false, // noindex
+    follow: false, // nofollow
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,

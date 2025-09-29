404c3711aa234db36a102ba25e00c6a7fa8624f7
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2025-09-29 17:05:53 +0100

    refactor: trigger transition only if setTimeout is present

--- frontend/src/hooks/useTransitionTrigger.ts
+++ frontend/src/hooks/useTransitionTrigger.ts
@@ -5,9 +5,13 @@ import React from 'react';
 export function useTransitionTrigger(delay: number) {
   const [isTriggered, setTrigger] = React.useState(false);
   React.useEffect(() => {
-    window.setTimeout(() => {
+    if (window != null && window?.setTimeout != null) {
+      window.setTimeout(() => {
+        setTrigger(true);
+      }, delay);
+    } else {
       setTrigger(true);
-    }, delay);
+    }
   });
 
   return {

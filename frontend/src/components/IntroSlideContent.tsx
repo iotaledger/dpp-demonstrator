f89661444edd37ebc6e59a5d3880c40d81a4b7f9
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2025-10-01 14:59:38 +0100

    refactor: adjust slide content vertical centering

--- frontend/src/components/IntroSlideContent.tsx
+++ frontend/src/components/IntroSlideContent.tsx
@@ -9,7 +9,7 @@ const IntroSlideContent: React.FC<IntroSlideContentProps> = ({
 }) => {
   return (
     <div
-      className="max-w-8xl grid w-full max-h-full lg:mt-[-7%] mx-auto backdrop-blur-sm rounded-[2.5rem] px-6 md:px-8 lg:px-12 pt-6 md:pt-8 lg:pt-12 pb-8 bg-transparent text-gray-800"
+      className="max-w-8xl grid w-full max-h-full lg:mt-[-2%] mx-auto backdrop-blur-sm rounded-[2.5rem] px-6 md:px-8 lg:px-12 pt-6 md:pt-8 lg:pt-12 pb-8 bg-transparent text-gray-800"
       style={{
         transition: 'background-color 0.6s ease-out, color 0.6s ease-out'
       }}

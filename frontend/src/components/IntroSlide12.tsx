929cbfbc3e462a5bb50c69581d126a83c2243b03
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2025-10-01 16:29:22 +0100

    fix: action response when using from nightly wallet

--- frontend/src/components/IntroSlide12.tsx
+++ frontend/src/components/IntroSlide12.tsx
@@ -38,27 +38,13 @@ const IntroSlide12: React.FC = () => {
             <svg className='inline align-baseline' xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 3h6v6" /><path d="M10 14 21 3" /><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /></svg>
           </Link>
         </SlideDescription>
-        {/* <ActionButtons
+        <ActionButtons
           primaryButton={{
             text: "Start Tour",
             onClick: handleStartGuidedTour,
             variant: "primary"
           }}
-        />*/}
-        <div className="py-6 flex flex-col md:flex-row gap-6 items-center">
-          <Link
-            href={"/explore-guided"}
-            className={'pointer-coarse:hidden w-fit bg-blue-600 text-white hover:bg-blue-700 px-8 py-4 rounded-full text-lg font-medium transition-all cursor-pointer duration-300  hover:shadow-lg transform hover:scale-102'}
-          >
-            {"Start Tour"}
-          </Link>
-          <Link
-            href={"nightly://v1?network=iota&url=https://dpp-demostrator-prod.if4testing.rocks/explore-guided"}
-            className={'not-pointer-coarse:hidden w-fit bg-blue-600 text-white hover:bg-blue-700 px-8 py-4 rounded-full text-lg font-medium transition-all cursor-pointer duration-300  hover:shadow-lg transform hover:scale-102'}
-          >
-            {"Start Tour"}
-          </Link>
-        </div>
+        />
       </SlideContent>
     </ImageTextLayout>
   );

c31535aa5480ecd019dc862a8422c1591c6cc670
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2025-10-31 18:26:38 +0000

    chore: remove frontend-legacy-demonstrator project

--- frontend-legacy-demonstrator/src/helpers/roleToEntryType.ts
+++
@@ -1,9 +0,0 @@
-type T = (key: string, params?: { [key: string]: string | number }) => string
-
-export default function (role: string, t: T) {
-  if (role === 'Repairer') {
-    return t('Repairment')
-  }
-
-  return undefined
-}

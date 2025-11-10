c3464aab31b4e6672b4a7c163b81db3e99353e79
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2025-11-10 14:03:50 +0000

    chore: configure i18n instantiation

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,17 @@
+import i18next from "i18next";
+import Backend from 'i18next-http-backend';
+
+i18next
+  .use(Backend)
+  .init({
+    backend: {
+      loadPath: '/locales/{{lng}}.json'
+    },
+    defaultNS: false,
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+export default i18next;
